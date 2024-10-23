2087ac7036de3e476d56d6ca0b548760bdf485b7
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-10-23 16:54:24 -0400

    tests with cypress

---
+++ cypress/e2e/crearObj.cy.js
@@ -0,0 +1,13 @@
+describe("Crear Objetivo", () => {
+    it("Debería mostrar el objetivo creado al usuario", () => {
+      cy.visit("/");
+  
+      cy.get("#objName").type("Vacaciones");
+      cy.get("#objMount").type(1500);
+      cy.get("#objDate").type("2025-02-10");
+      
+      cy.get("#obj-button").click();
+  
+      cy.get("#resultado-div").should("contain", "Vacaciones").and("contain", "1500").and("contain","2025-02-10");
+    });
+  });
