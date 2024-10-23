fc81656ee836063c78ac1746bad84649178ef16f
diegooferruuu <diego.ferrufino@ucb.edu.bo>
2024-10-23 17:03:21 -0400

    Refactorizacion

--- src/gastos/gastos.test.js
+++ src/gastos/gastos.test.js
@@ -6,8 +6,31 @@ describe("Registro de Gastos", () => {
     expect(gasto.monto).toEqual(100);
   });
 
+  it("deberia devolver la fecha 31/01/2020", () => {
+    const gasto = new Gasto("31/01/2020", "Compra libros", 300);
+    expect(gasto.fecha).toEqual("31/01/2020");
+  });
+
   it("deberia devolver la fecha, descripcion y monto adecuados", () => {
-    const gasto = new Gasto("25/11/2023", "Compra comida", 100);
-    expect(gasto.registrarGasto()).toEqual({'fecha': "25/11/2023", 'descripcion': "Compra comida", 'monto': 100});
+    let fecha = "25/11/2023";
+    let desc = "Compra comida";
+    let monto = 100;
+    const gasto = new Gasto(fecha, desc, monto);
+    expect(gasto.registrarGasto()).toEqual({
+      'fecha': "25/11/2023", 
+      'descripcion': "Compra comida", 
+      'monto': 100});
+  });
+
+  it("debería devolver una fecha distinta", () => {
+    let fecha = "01/01/2024";
+    let desc = "Compra bebida";
+    let monto = 50;
+    const gasto = new Gasto(fecha, desc, monto);
+    expect(gasto.registrarGasto()).toEqual({
+      'fecha': "01/01/2024", 
+      'descripcion': "Compra bebida", 
+      'monto': 50
+    });
   });
 });
