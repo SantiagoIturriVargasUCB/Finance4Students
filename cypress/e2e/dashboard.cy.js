e8a51d6ad8829c455a59a05152d21dba8c42376a
Ichurri <santiagoiturrivargas04@gmail.com>
2024-10-23 16:29:47 -0400

    add dashborad cypress tests

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,22 @@
+describe('Dashboard Functionality', () => {
+    beforeEach(() => {
+      cy.visit('src/dashboard/dashboard.html');  // Asegúrate de que la ruta sea correcta
+    });
+  
+    // Caso 1: Verificar que el dashboard muestra el mensaje de bienvenida correcto
+    it('should display the correct welcome message', () => {
+      cy.get('h1').should('contain', 'Welcome, validuser@example.com!');
+    });
+  
+    // Caso 2: Verificar que el botón de logout existe
+    it('should have a logout button', () => {
+      cy.get('#logoutBtn').should('exist');
+    });
+  
+    // Caso 3: Simular el clic en logout y redirigir a la página de login
+    it('should redirect to login after clicking logout', () => {
+      cy.get('#logoutBtn').click();
+      cy.url().should('include', '/LogIn/login.html');
+    });
+  });
+  
