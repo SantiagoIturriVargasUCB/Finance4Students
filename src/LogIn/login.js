8f52794a62b6dc49b1a535a39f200f3d7bdf79f0
Ichurri <santiagoiturrivargas04@gmail.com>
2024-10-23 14:43:11 -0400

    create view for login

---
+++ src/LogIn/login.js
@@ -0,0 +1,27 @@
+class Auth {
+  constructor() {
+    this.validEmail = 'validuser@example.com';
+    this.validPassword = 'ValidPassword123';
+  }
+
+  authenticate(email, password) {
+    if (!email || !password) return false;
+
+    return email === this.validEmail && password === this.validPassword;
+  }
+}
+
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();  
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const auth = new Auth();
+  const isAuthenticated = auth.authenticate(email, password);
+
+  if (isAuthenticated) window.location.href = '../dashboard/dashboard.html';
+  else document.getElementById('errorMessage').textContent = 'Invalid email or password';
+});
