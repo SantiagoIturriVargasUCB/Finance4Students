9eaf69df1c13524de809968c69d63ef393ecd137
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-10-23 14:48:34 -0400

    show all obj.

--- src/crearObjAhorro/objAhorro.test.js
+++ src/crearObjAhorro/objAhorro.test.js
@@ -28,7 +28,17 @@ describe("Objetivo de Ahorro", () => {
         });
     });
 
-    it("Deberia devolver un objetivo dado un objeto", () => {
+    it("Debería devolver un objetivo dado un objeto", () => {
+        const obj = new Objetivo();
+        const newObj = {
+            objName: "Viaje",
+            mount: 2000,
+            date: "10/01/2026"
+        };
+        expect(obj.crearObjetivo(newObj)).toEqual(newObj);
+    });
+
+    it("Debería almacenar y mostrar los objetivos creados", () => {
         const objs = new Objetivo();
         const newObj = {
             objName: "Vacaciones",
@@ -38,8 +48,28 @@ describe("Objetivo de Ahorro", () => {
 
         objs.crearObjetivo(newObj);
 
-        let objCreated = objs.showObj();
-        expect(objCreated).toEqual(objs.showObj());
+        const storedObjs = objs.showObj(); 
+        expect(storedObjs).toEqual([newObj]);
+    });
+
+    it("Deberia poder crear y mostrar varios objetivos", () => {
+        const objs = new Objetivo();
+        const objNavidad = {
+            objName: "Navidad",
+            mount: 10000,
+            date: "12/25/2024"
+        };
+        objs.crearObjetivo(objNavidad); 
+        
+        const objCumple = {
+            objName: "Cumple",
+            mount: 2000,
+            date: "11/14/2024"
+        };
+        objs.crearObjetivo(objCumple);
+        
+        const storedObjs = objs.showObj()
+        expect(storedObjs).toEqual([objNavidad, objCumple])
     })
 });
 
