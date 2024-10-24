263ad183bd350fd91281d6e19eda4180fb7df9c3
Camilo Padilla <camilo.padilla@ucb.edu.bo>
2024-10-24 00:02:28 -0400

    Refactorizacion historial de Ingresos cypress

--- cypress/e2e/historialDeIngresos.cy.js
+++ cypress/e2e/historialDeIngresos.cy.js
@@ -2,26 +2,26 @@ describe('Prueba del historial de ingresos', () => {
 
     it('Debería agregar un ingreso y mostrarlo en la lista', () => {
         cy.visit("/src/registrarIngreso/registroIngreso.html"); 
-        cy.get('#monto').type('500');
-        cy.get('#fecha').type('2024-10-23');
-        cy.get('#descripcion').type('Sueldo');
+        cy.get('#monto').type(500);
+        cy.get('#fecha').type("2024-10-23");
+        cy.get('#descripcion').type("Sueldo");
         cy.get('#insert-button').click();
-        cy.get('#ingreso-div').should('contain', 'Monto:');
-        cy.get('#ingreso-div').should('contain', 'Fecha:');
-        cy.get('#ingreso-div').should('contain', 'Descripcion:');
+        cy.get('#ingreso-div').should('contain', 500)
+        .and('contain', "2024-10-23")
+        .and('contain', "Sueldo");
     });
 
     it('Debería mostrar el historial de ingresos cuando se hace clic en "Ver Historial"', () => {
         cy.visit("/src/registrarIngreso/registroIngreso.html"); 
-        cy.get('#monto').type('500');
-        cy.get('#fecha').type('2024-10-23');
-        cy.get('#descripcion').type('Sueldo');
+        cy.get('#monto').type(500);
+        cy.get('#fecha').type("2024-10-23");
+        cy.get('#descripcion').type("Sueldo");
         cy.get('#insert-button').click();
 
         cy.get('#ver-historial').click();
 
-        cy.get('#historial-lista').should('contain', '500');
-        cy.get('#historial-lista').should('contain', '2024-10-23');
-        cy.get('#historial-lista').should('contain', 'Sueldo');
+        cy.get('#historial-lista').should('contain', 500)
+        .and('contain', "2024-10-23")
+        .and('contain', "Sueldo");
     });
 });
